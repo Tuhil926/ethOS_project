920f20b0793c9ec39d91147f290fdc53bd47a08b
Tuhil926 <kvtuhil@gmail.com>
2023-06-27 01:21:33 +0530

    made the multiplayer work

--- client.py
+++ client.py
@@ -1,5 +1,25 @@
 import socket
 import threading
+import pygame
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+inputs = [False, False, False, False]
+pos = [0, 0]
+other_positions = [[0, 0]]
+
+
+def make_input_data(input_list):
+    data = b""
+    for i in range(4):
+        if input_list[i]:
+            data += b"1 "
+        else:
+            data += b"0 "
+    
+    return data
 
 
 class Client:
@@ -16,13 +36,54 @@ class Client:
         self.server_sock.connect((self.server_host, self.server_port))
 
     def send_data(self):
+        global pos
         while self.running:
-            self.server_sock.sendall(b"hello world!!")
-            data = self.server_sock.recv(1024)
+            self.server_sock.sendall(make_input_data(inputs))
+            data = str(self.server_sock.recv(1024))[2:-1]
             print("received data:", data)
+            pos1 = [float(x) for x in data.split()]
+            pos[0] = pos1[0]
+            pos[1] = pos1[1]
+            if len(pos1) > 2:
+                other_positions[0] = [pos1[2], pos1[3]]
+
+
 
 
 client = Client()
 client.connect()
 print("connected")
-client.send_data()
+
+socket_thread = threading.Thread(target=client.send_data)
+socket_thread.start()
+running = True
+while running:
+    screen.fill((0, 0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        inputs[0] = True
+    else:
+        inputs[0] = False
+    if keys[pygame.K_a]:
+        inputs[1] = True
+    else:
+        inputs[1] = False
+    if keys[pygame.K_s]:
+        inputs[2] = True
+    else:
+        inputs[2] = False
+    if keys[pygame.K_d]:
+        inputs[3] = True
+    else:
+        inputs[3] = False
+    
+    pygame.draw.circle(screen, (255, 255, 255), pos, 20)
+
+    for other_pos in other_positions:
+        pygame.draw.circle(screen, (255, 255, 255), other_pos, 20)
+
+    pygame.display.update()
--- server.py
+++ server.py
@@ -2,11 +2,32 @@ import threading
 import socket
 
 
+inputs = [False, False, False, False]
+
+
 class Client_api:
     def __init__(self, address, conn, data) -> None:
         self.address = address
         self.conn = conn
         self.data = data
+        self.pos = [0, 0]
+    
+    def make_pos_to_send(self):
+        data = b""
+        data += str(self.pos[0]).encode() + b" "
+        data += str(self.pos[1]).encode() + b" "
+        return data
+
+    def parse_inputs(self):
+        inps = [int(x) for x in self.data.split()]
+        if inps[0]:
+            self.pos[1] -= 1
+        if inps[1]:
+            self.pos[0] -= 1
+        if inps[2]:
+            self.pos[1] += 1
+        if inps[3]:
+            self.pos[0] += 1
 
 
 class Server:
@@ -16,6 +37,7 @@ class Server:
     def __init__(self) -> None:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind(("0.0.0.0", 4000))
+        print(socket.gethostbyname(socket.gethostname()))
         listen_thread = threading.Thread(target=self.listen_for_connections)
         listen_thread.start()
 
@@ -28,13 +50,20 @@ class Server:
 
     def handle_connections(self):
         for client in self.clients:
-            print("waiting to recv data")
+            # print("waiting to recv data")
             data = client.conn.recv(1024)
             print("received data:" + str(data))
-            client.data += str(data)
-            client.conn.sendall(b"lol nice")
+            client.data = str(data)[2:-1]
+            client.parse_inputs()
+
+            to_send = client.make_pos_to_send()
+            for client2 in self.clients:
+                if client2 is not client:
+                    to_send += client2.make_pos_to_send()
+            client.conn.sendall(to_send)
 
 
 server = Server()
 while True:
     server.handle_connections()
+
